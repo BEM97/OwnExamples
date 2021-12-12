d310bc6e299e429d4c968ce2a54c827e75e009fe
“BEM97” <meesbem@gmail.com>
2021-12-12 17:44:51 +0330

    a

--- js-examples/scripts.js
+++ js-examples/scripts.js
@@ -1,3 +1,9 @@
 function biggerText(){
     document.getElementById("myP").style.fontSize = "55px";
 }
+
+let text1 ="mamd";
+let text2 = "reza";
+let text3 = text1 + " " + text2;
+document.getElementById("a").innerHTML=text3;
+
