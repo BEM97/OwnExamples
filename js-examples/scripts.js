7db1aa5570176a2a315f347f4a12653763b9a08f
“BEM97” <meesbem@gmail.com>
2021-12-15 17:24:52 +0330

    14000924

--- js-examples/scripts.js
+++ js-examples/scripts.js
@@ -299,6 +299,531 @@ let result = text1.replace(/w3schools/i,"mmad");
 
 
 
-document.getElementById("a").innerHTML = fruits.join(" * ");
-document.getElementById("b").innerHTML = a2;
+ //this keyword
+const person = {
+    firstName: "mamd",
+    lastName: "reza",
+    id      : 112,
+    fullName:function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
 
+
+//person.fullName()
+
+
+// this Alone
+// When used alone, the owner is the Global object, so this refers to the Global object.
+
+// In a browser window the Global object is [object Window]:
+//  let XX =this;
+
+
+
+
+
+
+
+//classes 
+
+
+class Car{
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+    age(){
+        let date =new Date();
+        return date.getFullYear()-this.year;
+    }
+}
+
+let mycar = new Car("Ford",2010);
+
+//mycar.age();
+// mycar.name;
+
+
+
+
+
+//JSON
+
+let teXT = '{"employees":['+
+    '{"firstName":"mamd","lastName":"reza"},'+
+    '{"firstName":"ali","lastName":"reza"},'+
+    '{"firstName":"amir","lastName":"hosen"}]}';
+
+const obj = JSON.parse(teXT);
+
+
+//obj.employees[0].firstName // mmad
+
+
+
+
+// debugger;
+// create breakpoint for debugging in brwoser
+
+
+
+
+
+// js function call  
+//With the call() method, you can write a method that can be used on different objects.
+
+const Person = {
+    fullName: function(city,country){
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+}
+
+const person1={
+    firstName:"mmad",
+    lastName:"reza"
+}
+
+// person.fullName.call(person1,"tehran","iran");
+
+
+
+
+
+
+
+
+
+
+// js function apply  
+// The apply() method is similar to the call() method  but with array argoman
+
+
+// Math.max(1,2,3); //return 3 
+
+// Since JavaScript arrays do not have a max() method, you can apply the Math.max() method instead.
+
+
+// Math,max.apply(null,[1,2,3,]); // will return 3 null can replace " " or 0 or Math
+
+
+
+
+
+
+// js function closure
+
+
+const add = (function (){
+let counter = 0;
+return function(){counter +=1;return counter}
+})();
+
+// (); end of function is selfinvoking //function call by self  
+
+
+
+
+
+
+
+
+// Class Inheritance
+
+class Car1 {
+    constructor(brand){
+      this.carname = brand;
+    }
+    present(){
+        return 'I have a ' + this.carname;
+    }
+}
+
+
+class Model extends Car1 {
+    constructor(brand, mod){
+        super(brand);
+        this.model = mod;
+    }
+    show(){
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+
+
+let myCar = new Model ("saipa","pride");
+
+
+//myCar.show();
+
+
+
+
+
+//getter & setter
+
+class Car2 {
+    constructor(brand){
+        this.carname = brand;
+    }
+    get getCnam(){
+      return this.carname;
+    }
+    set setCnam(x){
+        this.carname = x;
+    }
+}
+
+let mmadCar = new Car2("irankhodro");
+
+
+// mmadCar.getCnam;
+
+
+ // getter notbe used with ();  
+//Note: even if the getter is a method, you do not use parentheses 
+// when you want to get the property value.
+
+
+// mmadCar.setCnam("saipa"); // incorrect
+ 
+mmadCar.setCnam="saipa";   // not use ()
+
+
+
+
+
+
+
+//js callback
+
+
+// function myDisplayer(some){
+// document.getElementById("b").innerHTML =some;
+
+// }
+
+
+// function myCalculator(num1, num2, myCallback){
+//     let = sum = num1 + num2;
+//     myCallback(sum);
+// }
+
+
+// myCalculator(5, 6, myDisplayer); 
+
+
+//When you pass a function as an argument, remember not to use parenthesis.
+// Right: myCalculator(5, 5, myDisplayer);
+// Wrong: myCalculator(5, 5, myDisplayer());
+
+
+
+
+
+
+
+
+//Asynchronous JavaScript
+setTimeout(myfunction10,3000);
+function myfunction10(){
+    // document.getElementById("b").innerHTML="hi iam mmad!";
+}
+
+
+// In the example above, myFunction is used as a callback.
+// myFunction is passed to setTimeout() as an argument.
+// 3000 is the number of milliseconds before time-out, so myFunction() will be called after 3 seconds.
+// When you pass a function as an argument, remember not to use parenthesis.
+// Right: setTimeout(myFunction, 3000);
+// Wrong: setTimeout(myFunction(), 3000);
+
+
+
+
+
+
+
+
+// using setInterval fo display time every seconds
+
+
+setInterval(myFunction11,1000);
+
+function myFunction11(){
+    let d = new Date();
+//   document.getElementById("b").innerHTML=
+//   d.getHours() + ":" +
+//   d.getMinutes() + ":" +
+//   d.getSeconds();
+
+}
+
+
+
+//use callback for waitting for files
+
+
+
+
+
+
+
+
+
+
+
+//js Promise
+
+// function myDisplayer1(some){
+//     // document.getElementById("b").innerHTML=some;
+// }
+
+
+// let myPromise = new Promise(function(myResolve, myReject){
+  
+//     let x = 0;
+
+//     if (x==0){
+//         myResolve("ok");
+//     }else{
+//         myReject("Error");
+//     }
+
+// });
+
+
+// myPromise.then(
+//     function(value){myDisplayer(value);},
+//     function(error) {myDisplayer(error);}
+// );
+
+
+
+
+//set a timout with promise
+
+// let myPromise = new Promise(function(myResolve, myReject){
+
+//     setTimeout(function(){myResolve("I am mmad");},3000);
+
+
+// });
+
+
+
+// myPromise.then(function(value){
+//     document.getElementById("b").innerHTML=value;
+// });
+
+
+
+
+
+
+
+
+
+
+
+//js Async & await
+
+//setTimeout wit Async & await
+
+
+// async function myDisplay(){
+  
+//     let myPromise =new Promise (function(resolve){
+//         setTimeout(function(){resolve("i am mmad");},3000);
+//     });
+
+//     document.getElementById("b").innerHTML = await myPromise;
+// }
+
+// myDisplay();
+
+
+
+
+
+
+
+
+
+
+//js validation
+
+
+// (function inputValidate(){
+    
+//     // let x =document.getElementById("num").value;
+//     let x = 2;
+    
+//     let text;
+//     if(isNaN(x) || x<1 || x>10 ){
+//         text = "Input not valid";
+//     }else{
+//         text = "input ok";
+//     }
+//     document.getElementById("b").innerHTML=text;
+// })();
+
+
+
+
+
+
+
+
+
+//js web api 
+// geolcation api
+
+// const myElement = document.getElementById("c");
+
+// (function geoLocation(){
+//     if (navigator.geolocation){
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     }else{
+//         myElement.innerHTML = "geolocation is not supported by browser";
+//     }
+// })();
+
+// function showPosition(position){
+//     myElement.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch Api
+
+// getText("a.txt");
+
+// async function getText(file){
+//     let myObject = await fetch(file);
+//     let myText = await myObject.text();
+//     document.getElementById("c").innerHTML = myText;
+// }
+
+
+
+
+
+// fetch(file)
+// .then(x => x.text())
+// .then(y => myDisplay(y));
+
+
+
+
+
+
+    // fetch ('https://33ed6528-6a3f-4e2c-b95a-a38f65fa379e.mock.pstmn.io/b.txt')
+    // .then(res => res.text())
+    // // .then(res => console.log(res))    
+    // .then(text => document.getElementById("c").innerHTML= text);
+
+
+
+
+
+
+
+    // fetch ('https://9b7cf01d-4c23-4c3a-b888-a5cb1c15923c.mock.pstmn.io/c')
+    // .then(res => res.json())
+    // // .then(res => console.log(res))    
+    // .then(text => document.getElementById("c").innerHTML= JSON.stringify(text));
+
+
+
+
+
+   
+
+
+    // fetch('https://api.freerealapi.com/blogs')
+    // .then(res => res.json())
+  
+    // // .then(json => console.log(json))
+    // .then(json => document.getElementById("c").innerHTML=JSON.stringify(json));
+
+
+
+
+
+
+
+
+
+
+//AJAX XMLHttpRequest
+
+function getText(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        document.getElementById("c").innerHTML = this.responseText;
+    }
+    xhttp.open("GET","a.txt",true);
+    xhttp.send();
+}
+
+// getText();
+
+
+
+
+
+
+
+function loadDoc(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+         document.getElementById("c").innerHTML = this.responseText;   
+        //  console.log(this.status);
+        //  console.log(this.readyState);
+        }
+    }
+    xhttp.open("GET","https://33ed6528-6a3f-4e2c-b95a-a38f65fa379e.mock.pstmn.io/b.txt",true); 
+
+    // in url a.txt = http://127.0.0.1:5500/js-examples/a.txt =  http://localhost:5500/js-examples/a.txt
+    xhttp.send();
+}
+//https://fc7a4b2b-3b09-4962-8e59-234aa9479f01.mock.pstmn.io
+
+// loadDoc();
+
+// By sending asynchronously, the JavaScript does not have to wait for
+//  the server response, but can instead:
+
+// execute other scripts while waiting for server response
+// deal with the response after the response is ready
+// The default value for the async parameter is async = true.
+
+// You can safely remove the third parameter from your code.
+
+// Synchronous XMLHttpRequest (async = false) is not recommended because 
+// the JavaScript will stop executing until the server response is ready.
+// If the server is busy or slow, 
+// the application will hang or stop.
+
+
+
+
+
+
+
+
+
+
+document.getElementById("a").innerHTML = fruits.join(" * ");
+// document.getElementById("b").innerHTML =mmadCar.getCnam;
