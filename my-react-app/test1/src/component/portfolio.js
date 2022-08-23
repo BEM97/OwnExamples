81336d2aa390d4058a7be10b756263a173f1f671
“BEM97” <meesbem@gmail.com>
2022-08-23 13:02:10 +0430

    140106011301

--- my-react-app/test1/src/component/portfolio.js
+++ my-react-app/test1/src/component/portfolio.js
@@ -1,54 +1,50 @@
+const header = {
+  homepage: "https://github.com",
+  title: "react.",
+};
 
-const header ={
-  homepage:'https://github.com',
-  title:'react.'
-}
-
-const about ={
-  name:'mohammadreza esmailian',
-  role:'junior frontend developer :)',
-  description:'i am new in FrontEnad and so happy learn more with  you :)',
-  resume:'https://Linkedin.com',
+const about = {
+  name: "mohammadreza esmailian",
+  role: "junior frontend developer :)",
+  description: "i am new in FrontEnad and so happy learn more with  you :)",
+  resume: "https://jobinja.ir/user/WB-5693924",
   social: {
-    linkedin:'https://linkedin.com',
-    github:'https://github.com',
+    linkedin: "https://www.linkedin.com/in/mohammadreza-esmailian-abb307206",
+    github: "https://github.com/BEM97",
   },
-
-}
-
-
+};
 
 const projects = [
   {
-    name:'WeatherApp',
-    description: 'simple app forcast weather',
-    stack:['React' ,'javascript'],
-    sourceCode:'https://github.com',
-    livePreview:'https://github.com',
-
-
+    name: "Portfolio",
+    description: "This site you visited.",
+    stack: ["React", "javascript", "css"],
+    sourceCode:
+      "https://github.com/BEM97/OwnExamples/tree/main/my-react-app/test1",
+    livePreview: "https://bem97.github.io/",
   },
   {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  }
+    name: "WeatherApp",
+    description: "simple app forcast weather",
+    stack: ["React", "javascript", "css"],
+    sourceCode:
+      "https://github.com/BEM97/OwnExamples/tree/main/my-react-app/weatherapp",
+    livePreview: "https://bem97.github.io/weatherapp/",
+  },
+  {
+    name: "TodoApp",
+    description: "Add todo checked when done ",
+    stack: ["React", "javascript", "css"],
+    sourceCode:
+      "https://github.com/BEM97/OwnExamples/tree/main/my-react-app/todoapp",
+    livePreview: "https://bem97.github.io/todoapp/",
+  },
 ];
 
-const contact ={
-  email:'meesbem@gmail.com'
-}
+const contact = {
+  email: "meesbem@gmail.com",
+};
 
-const skills =[
-  'HTML',
-  'CSS',
-  'javascript',
-  'React',
-  'Git',
-  'SASS',
-]
+const skills = ["HTML", "CSS", "SASS", "javascript", "React", "Git"];
 
-export { projects,contact,about, skills, header};
+export { projects, contact, about, skills, header };
--- my-react-app/test1/src/component/projectss/projects.js
+++ my-react-app/test1/src/component/projectss/projects.js
@@ -8,6 +8,10 @@ const Projects = () => {
   return (
     <section id="projects" className="sectionp projects">
       <h2 className="projects__title">Projects</h2>
+      <h3 className="projects__desc">
+        this projects clone from <a href="https://reactjsexample.com/">reactjsexample.com</a> for practice
+        and Understand concepts Rewritten by myself
+      </h3>
       <div className="projects__grid">
         {projects.map((project) => (
           <Projectcontainer key={uniqid()} project={project} />
